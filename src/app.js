344153010dd97ba89c7b9399a9fb2fe73f3e351d
rohan2734 <jagandevaki1@gmail.com>
2021-02-04 22:54:38 +0530

    added user form signup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const exphbs = require('express-handlebars');
+require("dotenv").config();
+const express = require("express");
+const exphbs = require("express-handlebars");
 const path = require("path");
 const hbs = require("hbs");
 const app = express();
@@ -8,12 +8,13 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const auth = require("./middleware/auth");
+const formidable = require("formidable");
+const fs = require("fs");
 
 require("./db/conn");
-const Register = require("./models/registers");
+const UserSchema = require("./models/users");
 
 const port = process.env.PORT || 3000;
-
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
@@ -21,127 +22,101 @@ const partials_path = path.join(__dirname, "../templates/partials");
 
 //app.use(express.static(static_path));
 
-
-
-
-
-
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "hbs");
 app.set("views", template_path);
 hbs.registerPartials(partials_path);
-
 
 app.use(express.json());
 //cookie-parser as middelware
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-    res.render("index")
+  res.render("index");
 });
 app.get("/login", (req, res) => {
-    res.render("login")
+  res.render("login");
 });
 
 app.get("/dashboard", auth, (req, res) => {
-    res.render("dashboard")
+  res.render("dashboard");
 });
 app.get("/register", (req, res) => {
-    res.render("register");
+  res.render("register");
+});
 
-})
+// app.post("/register", async(req, res) => {
+//         try {
 
-app.post("/register", async(req, res) => {
-        try {
+//             const password = req.body.password;
+//             const cpassword = req.body.confirmpassword;
 
-            const password = req.body.password;
-            const cpassword = req.body.confirmpassword;
+//             if (password === cpassword) {
 
+//                 const forklift = new Register({
+//                     firstname: req.body.firstname,
+//                     lastname: req.body.lastname,
+//                     email: req.body.email,
+//                     password: req.body.password,
+//                     confirmpassword: cpassword,
+//                     phone: req.body.phone,
+//                     upload: req.body.upload,
+//                     company: req.body.company
+//                 })
 
-            if (password === cpassword) {
+//                 //generate token
+//                 const token = await forklift.generateAuthToken();
+//                 //cookiess //
+//                 res.cookie("jwt", token, {
+//                     expires: new Date(Date.now() + 30000 * 60),
+//                     httpOnly: true
+//                 });
+//                 //res.cookie(name,value ,[options])
 
-                const forklift = new Register({
-                    firstname: req.body.firstname,
-                    lastname: req.body.lastname,
-                    email: req.body.email,
-                    password: req.body.password,
-                    confirmpassword: cpassword,
-                    phone: req.body.phone,
-                    upload: req.body.upload,
-                    company: req.body.company
-                })
+//                 const registered = await forklift.save();
 
+//                 res.status(201).render("login");
 
-                //generate token
-                const token = await forklift.generateAuthToken();
-                //cookiess //
-                res.cookie("jwt", token, {
-                    expires: new Date(Date.now() + 30000 * 60),
-                    httpOnly: true
-                });
-                //res.cookie(name,value ,[options])
+//             } else {
+//                 res.send("password must match");
+//             }
+//         } catch (error) {
+//             res.status(400).send(error);
+//         }
+//     })
+//     // login validation
+// app.post("/login", async(req, res) => {
+//     try {
 
+//         const email = req.body.email;
+//         const password = req.body.password;
 
-                const registered = await forklift.save();
+//         const useremail = await Register.findOne({ email: email });
 
-                res.status(201).render("login");
+//         const isMatch = await bcrypt.compare(password, useremail.password);
 
-            } else {
-                res.send("password must match");
-            }
-        } catch (error) {
-            res.status(400).send(error);
-        }
-    })
-    // login validation
-app.post("/login", async(req, res) => {
-    try {
+//         const token = await useremail.generateAuthToken();
+//         //jwt sent via cookies also called after login
+//         res.cookie("jwt", token, {
+//             expires: new Date(Date.now() + 30000 * 60),
+//             httpOnly: true
+//                 // secure: true
+//         });
 
-        const email = req.body.email;
-        const password = req.body.password;
+//         if (isMatch) {
+//             res.status(201).render("index");
 
-        const useremail = await Register.findOne({ email: email });
+//         } else {
+//             res.send("Email or Password is incorrect");
+//         }
 
-        const isMatch = await bcrypt.compare(password, useremail.password);
+//     } catch (error) {
+//         res.status(400).send("Invalid Login Details")
+//     }
 
-
-        const token = await useremail.generateAuthToken();
-        //jwt sent via cookies also called after login 
-        res.cookie("jwt", token, {
-            expires: new Date(Date.now() + 30000 * 60),
-            httpOnly: true
-                // secure: true
-        });
-
-
-        if (isMatch) {
-            res.status(201).render("index");
-
-        } else {
-            res.send("Email or Password is incorrect");
-        }
-
-
-
-
-    } catch (error) {
-        res.status(400).send("Invalid Login Details")
-    }
-
-
-
-})
-
-
-
-
-
-
+// })
 
 //securePassword("")
-
-
 
 /*
 const createToken = async() => {
@@ -158,14 +133,43 @@ const createToken = async() => {
 createToken();
 
 */
+app.post("/signup", (req, res, next) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
 
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image cannot be uploaded",
+      });
+    }
 
+    // const {
+    //   firstname,
+    //   lastname,
+    //   email,
+    //   password,
+    //   confirmpassword,
+    //   phone,
+    //   company,
+    // } = fields;
 
+    let newUser = new UserSchema(fields);
 
+    if (files.userFiles) {
+      newUser.userFiles.data = fs.readFileSync(files.userFiles.path);
+      newUser.userFiles.contentType = files.userFiles.type;
+    }
 
+    newUser
+      .save()
+      .then((data) => res.status(201).json({ newUser }))
+      .catch((err) => console.log(err));
 
-
+    res.status(201).json({ newUser });
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running at port no ${port}`);
-})
+  console.log(`Server is running at port no ${port}`);
+});
